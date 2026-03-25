c74d317a7e0f8ccef1297c0ec222121be26564ae
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2026-03-25 14:21:41 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './site.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
